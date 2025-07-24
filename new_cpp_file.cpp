4f2b6b3c11921bc653a4e887be8936a2a3140e9d
Zav1ks <138763576+Zav1ks@users.noreply.github.com>
2025-07-24 20:33:57 +0300

    Create new_cpp_file.cpp

---
+++ new_cpp_file.cpp
@@ -0,0 +1,12 @@
+	int main() {
+    // Create an object of the Car class
+    Car myCar("Toyota", "Camry", 2022);
+    // TEST test
+
+    // Call a member method to display details
+    myCar.displayDetails();
+    // Access a member variable using a getter method
+    std::cout << "My car's brand is: " << myCar.getBrand() << std::endl;
+
+    return 0;
+}
