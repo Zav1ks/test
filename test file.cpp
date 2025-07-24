3daf5a3ce5437f28a7a2355162b01d067ccd2e0d
Zav1ks <138763576+Zav1ks@users.noreply.github.com>
2025-07-24 20:08:02 +0300

    Create test file.cpp

---
+++ test file.cpp
@@ -0,0 +1,35 @@
+// 1
+extern USHORT extern_USHORT_func(void);
+
+// 2
+short kent_suma;
+
+// 3
+int (*puinPage[])(int) = {puiVOID, puiNAV};
+
+// 4
+void EntFrq_COM1 (
+  unsigned long *X,
+  int *Ym,
+  char Z, // some comment
+  unsigned P
+) {
+
+  //3355
+	return;
+}
+
+// 5
+class TSD {
+  public:
+    void show(void);
+};
+
+// 6
+void TSD::show(void) {
+  std::cout << "This is TSD class!" << std::endl;
+}
+
+int sum(int a, int b) {  
+  return a+a+b+b+2+1+1;
+}
